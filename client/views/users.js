01b55877290ec3d232b71e16b944e8203db6c305
Israel D. Canasa <raeldc@gmail.com>
2013-02-14 17:47:51 +0800

    Allow users to exit rooms.

--- client/views/users.js
+++ client/views/users.js
@@ -8,3 +8,30 @@ Template.users.users = function(){
 		}
 	}
 };
+
+Template.users.helpers({
+	'notLobby': function(){
+		return !Session.equals('room', 'lobby');
+	}
+});
+
+Template.users.events({
+	'click #exit-room': function(){
+		var room = Rooms.findOne(Session.get('room'));
+		if(room.users){
+			var users = [];
+			var i = 0;
+			room.users.forEach(function(el){
+				if(el != Meteor.userId()){
+					users.push(el);
+				}
+			});
+
+			Rooms.update(Session.get('room'), {$set:{
+				users: users
+			}});
+
+			Session.set('room', 'lobby');
+		}
+	}
+});

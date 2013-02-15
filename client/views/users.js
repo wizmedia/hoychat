01a11ca6c2cecfe8f740c9ce651f03ec7aff5174
Israel D. Canasa <raeldc@gmail.com>
2013-02-15 09:08:33 +0800

    Remove the empty selector object when finding users.

--- client/views/users.js
+++ client/views/users.js
@@ -2,7 +2,7 @@ Meteor.subscribe('allusers');
 
 Template.users.users = function(){
 	if(Session.equals('room', 'lobby')){
-		return Meteor.users.find({});
+		return Meteor.users.find();
 	}else{
 		var room = Rooms.findOne(Session.get('room'));
 		if(room){
